967ff3275713242068b93f2de3fcb8a88ef02ba3
vanderleiks <vanderleiksilva@gmail.com>
2024-02-25 15:07:40 -0300

    Add type Button in Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ export function Sidebar() {
     <aside className="flex flex-col gap-6 w-[300px] h-screen p-4">
       <Image src={"./assets/logo.svg"} width={50} height={50} alt="logo"/>
       <nav className="flex flex-col gap-6">
-        <a className="link-sidebar" href="#"><Home className="size-5 twitter-color" /> Home</a>
+        <a className="link-sidebar" href="/"><Home className="size-5 twitter-color" /> Home</a>
         <a className="link-sidebar" href="#"><Hash className="size-5" /> Explore</a>
         <a className="link-sidebar" href="#"><Bell className="size-5" /> Notifications</a>
         <a className="link-sidebar" href="#"><Mail className="size-5" /> Messages</a>
@@ -16,7 +16,7 @@ export function Sidebar() {
         <a className="link-sidebar" href="#"><User className="size-5" /> Profile</a>
         <a className="link-sidebar" href="#"> <MoreHorizontal className="size-5" /> More</a>
       </nav>
-      <Button>Tweet</Button>
+      <Button type="button">Tweet</Button>
     </aside>
   )
 }
